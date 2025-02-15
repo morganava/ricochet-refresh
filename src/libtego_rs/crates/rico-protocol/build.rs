82a4554b98ec8f76684a5b621e9a582c26d2dd7b
Morgan <morgan@torproject.org>
2025-02-15 14:29:36 +0000

    rico-protocol: import and build the protobuf .proto files

---
+++ src/libtego_rs/crates/rico-protocol/build.rs
@@ -0,0 +1,18 @@
+extern crate protobuf_codegen;
+
+fn main() {
+    protobuf_codegen::Codegen::new()
+        // use pure-rust parser to generate
+        .pure()
+        // All inputs and imports from the inputs must reside in `includes` directories.
+        .includes(&["src/v3/protos"])
+        // Inputs must reside in some of include paths.
+        .input("src/v3/protos/AuthHiddenService.proto")
+        .input("src/v3/protos/ChatChannel.proto")
+        .input("src/v3/protos/ContactRequestChannel.proto")
+        .input("src/v3/protos/ControlChannel.proto")
+        .input("src/v3/protos/FileChannel.proto")
+        // Specify output directory relative to Cargo output directory.
+        .out_dir("src/v3/protos")
+        .run_from_script();
+}
--- src/libtego_rs/crates/rico-protocol/src/lib.rs
+++ src/libtego_rs/crates/rico-protocol/src/lib.rs
@@ -1,5 +1,17 @@
 #![doc = include_str!("../README.md")]
 
+mod v3;
+
+// usage: https://github.com/stepancheg/rust-protobuf/blob/master/protobuf-examples/pure-vs-protoc/src/main.rs
+
+use protobuf::Message;
+
 pub fn func() -> () {
     println!("hello world");
+
+    use crate::v3::protos::AuthHiddenService::Packet;
+
+
+    let bytes: Vec<u8> = Default::default();
+    let _packet = Packet::parse_from_bytes(&bytes).unwrap();
 }
