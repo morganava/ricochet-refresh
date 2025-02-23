8a8dd77e04856bd30353df866bd1859534c0b15a
Morgan <morgan@torproject.org>
2025-02-23 18:54:29 +0000

    rico-protocol: implement write_to_vec() for auth_hidden_service packet types

--- src/libtego_rs/crates/rico-protocol/src/v3/message.rs
+++ src/libtego_rs/crates/rico-protocol/src/v3/message.rs
@@ -967,6 +967,42 @@ pub mod auth_hidden_service {
         Result(Result),
     }
 
+    impl Packet {
+        pub fn write_to_vec(&self, v:& mut Vec<u8>) -> std::result::Result<(), crate::Error> {
+            use protobuf::Message;
+            use crate::v3::protos;
+
+            let mut pb: protos::AuthHiddenService::Packet = Default::default();
+
+            match self {
+                Packet::Proof(proof) => {
+                    let signature = proof.signature();
+                    let service_id = proof.service_id();
+
+                    let mut proof = protos::AuthHiddenService::Proof::default();
+                    proof.signature = Some(signature.into());
+                    proof.service_id = Some(service_id.to_string());
+
+                    pb.proof = Some(proof).into();
+                },
+                Packet::Result(result) => {
+                    let accepted = result.accepted();
+                    let is_known_contact = result.is_known_contact().clone();
+
+                    let mut result = protos::AuthHiddenService::Result::default();
+                    result.accepted = Some(accepted);
+                    result.is_known_contact = is_known_contact;
+
+                    pb.result = Some(result).into();
+                }
+            }
+
+            // serialise
+            pb.write_to_vec(v).map_err(crate::Error::ProtobufError)?;
+            Ok(())
+        }
+    }
+
     impl TryFrom<&[u8]> for Packet {
         type Error = crate::Error;
 
@@ -996,18 +1032,15 @@ pub mod auth_hidden_service {
                         Err(_) => return Err(Self::Error::InvalidProtobufMessage),
                     };
 
-                    let proof = Proof{signature, service_id};
+                    let proof = Proof::new(signature, service_id)?;
                     Ok(Packet::Proof(proof))
                 },
                 (None, Some(result)) => {
                     let accepted = result.accepted.ok_or(Self::Error::InvalidProtobufMessage)?;
 
                     let is_known_contact = result.is_known_contact;
-                    if accepted && is_known_contact.is_none() {
-                        return Err(Self::Error::InvalidProtobufMessage);
-                    }
 
-                    let result = Result{accepted, is_known_contact};
+                    let result = Result::new(accepted, is_known_contact)?;
                     Ok(Packet::Result(result))
                 },
                 _ => Err(Self::Error::InvalidProtobufMessage),
@@ -1036,15 +1069,46 @@ pub mod auth_hidden_service {
     #[derive(Debug, PartialEq)]
     pub struct Proof {
         // TODO: spec doesn't explicitly say how many bytes the proof's signature is
-        pub signature: [u8; PROOF_SIGNATURE_SIZE],
-        pub service_id: tor_interface::tor_crypto::V3OnionServiceId,
+        signature: [u8; PROOF_SIGNATURE_SIZE],
+        service_id: tor_interface::tor_crypto::V3OnionServiceId,
+    }
+
+    impl Proof {
+        pub fn new(signature: [u8; PROOF_SIGNATURE_SIZE], service_id: tor_interface::tor_crypto::V3OnionServiceId) -> std::result::Result<Self, crate::Error> {
+            Ok(Self{signature, service_id})
+        }
+
+        pub fn signature(&self) -> &[u8; PROOF_SIGNATURE_SIZE] {
+            &self.signature
+        }
+
+        pub fn service_id(&self) -> &tor_interface::tor_crypto::V3OnionServiceId {
+            &self.service_id
+        }
     }
 
     #[derive(Debug, PartialEq)]
     pub struct Result {
-        pub accepted: bool,
+        accepted: bool,
         // TODO: is_known_contact must be present if accepted is true
-        pub is_known_contact: Option<bool>,
+        is_known_contact: Option<bool>,
+    }
+
+    impl Result {
+        pub fn new(accepted: bool, is_known_contact: Option<bool>) -> std::result::Result<Self, crate::Error> {
+            if accepted && is_known_contact.is_none() {
+                return Err(crate::Error::PacketConstructionFailed("is_known_contact must be present if accepted is true".to_string()));
+            }
+            Ok(Self{accepted, is_known_contact})
+        }
+
+        pub fn accepted(&self) -> bool {
+            self.accepted
+        }
+
+        pub fn is_known_contact(&self) -> &Option<bool> {
+            &self.is_known_contact
+        }
     }
 }
 
@@ -1543,6 +1607,68 @@ fn test_round_trip() -> anyhow::Result<()> {
         assert_eq!(packet_src, packet_dest);
     }
 
+    // AuthHiddenService Proof
+    {
+        println!("---");
+
+        let signature = [0u8; auth_hidden_service::PROOF_SIGNATURE_SIZE];
+        let private_key = tor_interface::tor_crypto::Ed25519PrivateKey::generate();
+        let service_id = tor_interface::tor_crypto::V3OnionServiceId::from_private_key(&private_key);
+
+        let proof = auth_hidden_service::Proof::new(signature, service_id)?;
+
+        println!("{proof:?}");
+
+        let packet_src = auth_hidden_service::Packet::Proof(proof);
+
+        println!("{packet_src:?}");
+
+        let mut bytes: Vec<u8> = Vec::default();
+        packet_src.write_to_vec(&mut bytes)?;
+
+        println!("{bytes:?}");
+
+        let packet_dest: auth_hidden_service::Packet = bytes.as_slice().try_into()?;
+
+        println!("{packet_dest:?}");
+        let mut bytes: Vec<u8> = Vec::default();
+        packet_dest.write_to_vec(&mut bytes)?;
+
+        println!("{bytes:?}");
+
+        assert_eq!(packet_src, packet_dest);
+    }
+
+    // AuthHiddenService Result
+    {
+        println!("---");
+
+        let accepted = false;
+        let is_known_contact = Some(false);
+
+        let result = auth_hidden_service::Result::new(accepted, is_known_contact)?;
+
+        println!("{result:?}");
+
+        let packet_src = auth_hidden_service::Packet::Result(result);
+
+        println!("{packet_src:?}");
+
+        let mut bytes: Vec<u8> = Vec::default();
+        packet_src.write_to_vec(&mut bytes)?;
+
+        println!("{bytes:?}");
+
+        let packet_dest: auth_hidden_service::Packet = bytes.as_slice().try_into()?;
+
+        println!("{packet_dest:?}");
+        let mut bytes: Vec<u8> = Vec::default();
+        packet_dest.write_to_vec(&mut bytes)?;
+
+        println!("{bytes:?}");
+
+        assert_eq!(packet_src, packet_dest);
+    }
 
     anyhow::bail!("test over");
 }
