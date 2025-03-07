93bfe10f3f93718a63da8c3adcd4407cf1c11e0e
Morgan <morgan@torproject.org>
2025-03-07 19:36:41 +0000

    build: only update header if different from previous to prevent full-recompilation of C++ code

--- src/libtego_rs/crates/tego-rs/build.rs
+++ src/libtego_rs/crates/tego-rs/build.rs
@@ -16,13 +16,26 @@ fn main() {
         Err(_) => panic!("CARGO_TARGET_DIR not set"),
     };
 
-    let header_file_path = target_dir.join("include").join("tego").join("tego.h");
+    let header_file_dir = target_dir.join("include").join("tego");
+    std::fs::create_dir_all(header_file_dir.clone()).unwrap();
+
+    let header_file_path = header_file_dir.join("tego.h");
     println!("cargo:rerun-if-changed={}", header_file_path.display());
+    let temp_file_path = std::env::temp_dir().join("tego.h");
 
     // generate libgosling.h C header
     match cbindgen::generate(&crate_dir) {
-        Ok(bindings) => bindings.write_to_file(header_file_path.clone().into_os_string()),
+        Ok(bindings) => bindings.write_to_file(temp_file_path.clone().into_os_string()),
         Err(cbindgen::Error::ParseSyntaxError { .. }) => return, // ignore in favor of cargo's syntax check
         Err(err) => panic!("{:?}", err),
     };
+
+    let prev_source = std::fs::read(header_file_path.as_path()).unwrap_or(Default::default());
+    let new_source = std::fs::read(temp_file_path.as_path()).unwrap();
+
+    if prev_source != new_source {
+        std::fs::rename(temp_file_path.as_path(), header_file_path.as_path()).unwrap();
+    } else {
+        std::fs::remove_file(temp_file_path.as_path()).unwrap();
+    }
 }
