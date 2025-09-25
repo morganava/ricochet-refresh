726672e1258f685e5c1ab7ecf43ff63d128c2ecb
Morgan <morgan@torproject.org>
2025-09-25 11:24:12 +0000

    rico-protocol: implemented auth hidden service Result packet test

--- src/libtego_rs/crates/rico-protocol/tests/v3/auth_hidden_service.rs
+++ src/libtego_rs/crates/rico-protocol/tests/v3/auth_hidden_service.rs
@@ -3,6 +3,7 @@ use anyhow::*;
 use tor_interface::tor_crypto::V3OnionServiceId;
 
 // internal
+use rico_protocol::v3::message::auth_hidden_service::Result;
 use rico_protocol::v3::message::auth_hidden_service::*;
 
 #[test]
@@ -24,6 +25,42 @@ fn test_proof_serialization() -> anyhow::Result<()> {
             )?,
         )?),
     )];
+
+    // ensure serialisation round trip
+    for (bytes, expected_packet) in valid_packets.iter() {
+        // verify raw bytes can be serialised to a packet
+        let packet: Packet = bytes.as_slice().try_into()?;
+        // verify the serialised packet equals our expected packet
+        assert_eq!(packet, *expected_packet);
+        // convert packet back into bytes
+        let packet_bytes: Vec<u8> = (&packet).try_into()?;
+        // verify the bytes -> packet -> bytes round-trips
+        assert_eq!(packet_bytes.as_slice(), bytes);
+
+        // ensure equivalent packets serialise to equivalent bytes
+        let expected_packet_bytes: Vec<u8> = expected_packet.try_into()?;
+        assert_eq!(packet_bytes, expected_packet_bytes);
+    }
+    Ok(())
+}
+
+#[test]
+fn test_result_serialization() -> anyhow::Result<()> {
+    // verify failure cases
+    assert!(Result::new(true, None).is_err());
+
+    // ensure serialisation round trip
+    let valid_packets: [(Vec<u8>, Packet); 3] = [
+        (
+            vec![18, 4, 8, 1, 16, 1],
+            Packet::Result(Result::new(true, Some(true))?),
+        ),
+        (
+            vec![18, 4, 8, 1, 16, 0],
+            Packet::Result(Result::new(true, Some(false))?),
+        ),
+        (vec![18, 2, 8, 0], Packet::Result(Result::new(false, None)?)),
+    ];
 
     // ensure serialisation round trip
     for (bytes, expected_packet) in valid_packets.iter() {
