821991dbd3addb4ea83ae68ef96bb8e70abcdf0b
Morgan <morgan@torproject.org>
2025-09-26 08:22:42 +0000

    rico-protocol: implemented file channel FileHeaderResponse packet test

--- src/libtego_rs/crates/rico-protocol/tests/v3/file_channel.rs
+++ src/libtego_rs/crates/rico-protocol/tests/v3/file_channel.rs
@@ -83,3 +83,34 @@ fn test_file_header_ack_serialization() -> Result<()> {
     }
     Ok(())
 }
+
+#[test]
+fn test_file_header_response_serialization() -> Result<()> {
+    // ensure serialisation round trip
+    let valid_packets: [(Vec<u8>, Packet); 2] = [
+        (
+            vec![26, 4, 8, 0, 16, 0],
+            Packet::FileHeaderResponse(FileHeaderResponse::new(0u32, Response::Accept)?),
+        ),
+        (
+            vec![26, 4, 8, 1, 16, 1],
+            Packet::FileHeaderResponse(FileHeaderResponse::new(1u32, Response::Reject)?),
+        ),
+    ];
+
+    for (bytes, expected_packet) in valid_packets.iter() {
+        // verify raw bytes can be serialised to a packet
+        let packet: Packet = bytes.as_slice().try_into()?;
+        // verify the serialised packet equals our expected packet
+        assert_eq!(packet, *expected_packet);
+        // convert packet back into bytes
+        let packet_bytes: Vec<u8> = (&packet).try_into()?;
+        // verify the bytes -> packet -> bytes round-trips
+        assert_eq!(packet_bytes.as_slice(), bytes);
+
+        // ensure equivalent packets serialise to equivalent bytes
+        let expected_packet_bytes: Vec<u8> = expected_packet.try_into()?;
+        assert_eq!(packet_bytes, expected_packet_bytes);
+    }
+    Ok(())
+}
