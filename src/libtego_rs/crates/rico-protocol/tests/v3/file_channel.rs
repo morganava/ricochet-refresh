dd7fc1efce5aed0a5ada183552f717febcb60cda
Morgan <morgan@torproject.org>
2025-09-26 08:44:44 +0000

    rico-protocol: implemented file channel FileTransferCompleteNotification packet test

--- src/libtego_rs/crates/rico-protocol/tests/v3/file_channel.rs
+++ src/libtego_rs/crates/rico-protocol/tests/v3/file_channel.rs
@@ -176,3 +176,47 @@ fn test_file_chunk_ack_serialization() -> Result<()> {
     }
     Ok(())
 }
+
+#[test]
+fn test_file_transfer_complete_notification_serialization() -> Result<()> {
+    // ensure serialisation round trip
+    let valid_packets: [(Vec<u8>, Packet); 3] = [
+        (
+            vec![50, 4, 8, 0, 16, 0],
+            Packet::FileTransferCompleteNotification(FileTransferCompleteNotification::new(
+                0u32,
+                FileTransferResult::Success,
+            )?),
+        ),
+        (
+            vec![50, 4, 8, 1, 16, 1],
+            Packet::FileTransferCompleteNotification(FileTransferCompleteNotification::new(
+                1u32,
+                FileTransferResult::Failure,
+            )?),
+        ),
+        (
+            vec![50, 4, 8, 2, 16, 2],
+            Packet::FileTransferCompleteNotification(FileTransferCompleteNotification::new(
+                2u32,
+                FileTransferResult::Cancelled,
+            )?),
+        ),
+    ];
+
+    for (bytes, expected_packet) in valid_packets.iter() {
+        // verify raw bytes can be serialised to a packet
+        let packet: Packet = bytes.as_slice().try_into()?;
+        // verify the serialised packet equals our expected packet
+        assert_eq!(packet, *expected_packet);
+        // convert packet back into bytes
+        let packet_bytes: Vec<u8> = (&packet).try_into()?;
+        // verify the bytes -> packet -> bytes round-trips
+        assert_eq!(packet_bytes.as_slice(), bytes);
+
+        // ensure equivalent packets serialise to equivalent bytes
+        let expected_packet_bytes: Vec<u8> = expected_packet.try_into()?;
+        assert_eq!(packet_bytes, expected_packet_bytes);
+    }
+    Ok(())
+}
