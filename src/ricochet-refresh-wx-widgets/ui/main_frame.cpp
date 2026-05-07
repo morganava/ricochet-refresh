87b9e790af8f540b662d9d2a9d00bac0c03e6679
Morgan <morgan@torproject.org>
2026-05-07 08:33:50 +0000

    ricochet-refresh-wx-widgets: create panels and save on MainFrame object

--- src/ricochet-refresh-wx-widgets/ui/main_frame.cpp
+++ src/ricochet-refresh-wx-widgets/ui/main_frame.cpp
@@ -7,6 +7,7 @@
 #include "enums.hpp"
 #include "general_settings_panel.hpp"
 #include "interface_settings_panel.hpp"
+#include "metrics.hpp"
 #include "settings_panel.hpp"
 #include "strings.hpp"
 #include "wrapped_static_text.hpp"
@@ -14,60 +15,11 @@
 MainFrame::MainFrame() : wxFrame(nullptr, wxID_ANY, Strings::MainFrame::title()) {
     this->setup_menubar();
 
-    // auto panel = new BootstrapPanel(this);
-    // panel->ShowDisconnected();
-    // panel->ShowConnecting();
-    // panel->ShowConnected();
+    auto sizer = new wxBoxSizer(wxHORIZONTAL);
+    this->setup_main_panels(sizer);
+    this->setup_overlay_panels(sizer);
 
-    // auto panel = new ConnectionStatusPanel(
-    //     this,
-    //     Strings::ConnectionStatusPanel::bundled_client_string("tor", "0.4.8.21"),
-    //     ConnectionStatus::Online
-    // );
-
-    // auto panel = new GeneralSettingsPanel(this);
-    // auto panel = new InterfaceSettingsPanel(this);
-    // auto panel = new ConnectionSettingsPanel(this);
-
-    // auto panel = new SettingsPanel(this);
-
-    const static ContactHandle contacts[8] = {
-        reinterpret_cast<ContactHandle>(0),
-        reinterpret_cast<ContactHandle>(1),
-        reinterpret_cast<ContactHandle>(2),
-        reinterpret_cast<ContactHandle>(3),
-        reinterpret_cast<ContactHandle>(4),
-        reinterpret_cast<ContactHandle>(5),
-        reinterpret_cast<ContactHandle>(6),
-        reinterpret_cast<ContactHandle>(7),
-    };
-    auto panel = new ConversationsPanel(this, std::span(contacts, 8));
-
-    // auto top_string =
-    //     "Ricochet Refresh est applicatio nuntiorum inter pares quae Tor utitur ad clientes connectendos. Cum Ricochet Refresh incipis, servitium Tor occultum in computatro tuo creat. Inscriptio huius servitii occulti est identitas tua anonyma in rete Tor et modus quo alii tecum communicare poterunt.";
-    // auto left_string =
-    //     "Ricochet Refresh is a peer-to-peer messenger app that uses Tor to connect clients. When you start Ricochet Refresh it creates a Tor hidden service on your computer. The address of this hidden service is your anonymous identity on the Tor network and how others will be able to communicate with you.";
-    // auto right_string =
-    //     "Ricochet Refresh is een peer-to-peer berichtenapp die Tor gebruikt om gebruikers met elkaar te verbinden. Wanneer je Ricochet Refresh start, wordt er een Tor hidden service op je computer aangemaakt. Het adres van deze hidden service is je anonieme identiteit op het Tor-netwerk en bepaalt hoe anderen met je kunnen communiceren.";
-
-    // auto panel = new wxPanel(this, wxID_ANY);
-
-    // auto top_text = new WrappedStaticText(panel, wxID_ANY, top_string);
-    // auto left_text = new WrappedStaticText(panel, wxID_ANY, left_string);
-    // auto right_text = new WrappedStaticText(panel, wxID_ANY, right_string);
-
-    // auto v_sizer = new wxBoxSizer(wxVERTICAL);
-
-    // auto h_sizer = new wxBoxSizer(wxHORIZONTAL);
-    // v_sizer->Add(top_text, 0, wxEXPAND | wxALL, 8);
-
-    // h_sizer->Add(left_text, 1, wxEXPAND | wxRIGHT, 4);
-    // h_sizer->Add(right_text, 1, wxEXPAND | wxLEFT, 4);
-
-    // v_sizer->Add(h_sizer, 0, wxEXPAND | wxLEFT | wxRIGHT | wxBOTTOM, 8);
-
-    // panel->SetSizer(v_sizer);
-
+    this->SetSizerAndFit(sizer);
     this->SetMinSize(wxSize(800, 600));
     this->SetSize(wxSize(800, 600));
 
@@ -194,4 +146,28 @@ void MainFrame::setup_menubar() {
     menu_bar->Append(help_menu, Strings::MainFrame::MenuBar::help());
 
     this->SetMenuBar(menu_bar);
+}
+
+void MainFrame::setup_main_panels(wxBoxSizer* sizer) {
+    auto bootstrap_panel = new BootstrapPanel(this);
+    bootstrap_panel->ShowDisconnected();
+    sizer->Add(bootstrap_panel, 1, wxEXPAND);
+    this->bootstrap_panel = bootstrap_panel;
+}
+
+void MainFrame::setup_overlay_panels(wxBoxSizer* sizer) {
+    // todo: ensure exiting these panels is consistent (e.g. with an 'Ok' button)
+    auto settings_panel = new SettingsPanel(this);
+    sizer->Add(settings_panel, 1, wxEXPAND);
+    settings_panel->Hide();
+    this->settings_panel = settings_panel;
+
+    auto connection_status_panel = new ConnectionStatusPanel(
+        this,
+        Strings::ConnectionStatusPanel::bundled_client_string("tor", "0.4.8.21"),
+        ConnectionStatus::Online
+    );
+    sizer->Add(connection_status_panel, 1, wxEXPAND);
+    connection_status_panel->Hide();
+    this->connection_status_panel = connection_status_panel;
 }
--- src/ricochet-refresh-wx-widgets/ui/main_frame.hpp
+++ src/ricochet-refresh-wx-widgets/ui/main_frame.hpp
@@ -6,9 +6,20 @@ public:
 
 private:
     void setup_menubar();
+    void setup_main_panels(wxBoxSizer* sizer);
+    void setup_overlay_panels(wxBoxSizer* sizer);
 
     // Event Handlers
     void on_exit(wxCommandEvent& event) {
         this->Close(true);
     }
+
+    // main panels
+    class BootstrapPanel* bootstrap_panel = nullptr;
+    // todo: implement this
+    class SessionsPanel* profile_notebook_panel = nullptr;
+
+    // overlay panels
+    class SettingsPanel* settings_panel = nullptr;
+    class ConnectionStatusPanel* connection_status_panel = nullptr;
 };
